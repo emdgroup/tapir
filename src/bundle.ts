44f5743701453b4e75c6ebdf9bd75390ee64097c
Moritz Onken <onken@netcubed.de>
2021-06-30 20:24:27 -0400

    ensure that the bundled version of ajv and ajv-formats is used

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,5 +1,12 @@
 import * as fs from 'fs';
 import { buildSync } from 'esbuild';
+import * as path from 'path';
+
+const ajvPath = require.resolve('ajv');
+let nodePath = ajvPath;
+while (path.basename(nodePath) !== 'node_modules') {
+    nodePath = path.resolve(nodePath, '..');
+}
 
 export function bundle(entrypoint: string, outfile: string): void {
     buildSync({
@@ -8,6 +15,7 @@ export function bundle(entrypoint: string, outfile: string): void {
         platform: 'node',
         format: 'cjs',
         bundle: true,
+        nodePaths: [nodePath],
     });
     fs.unlinkSync(entrypoint);
 }
